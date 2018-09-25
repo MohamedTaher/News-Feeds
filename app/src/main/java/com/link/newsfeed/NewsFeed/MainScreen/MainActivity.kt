0ab8bb9f01d7688482c92cf4be95c506a52b2220
Mohamed Taher <mohamedt@silverkeytech.com>
2018-09-26 00:21:23 +0200

    Add menu to main activity

--- app/src/main/java/com/link/newsfeed/NewsFeed/MainScreen/MainActivity.kt
+++ app/src/main/java/com/link/newsfeed/NewsFeed/MainScreen/MainActivity.kt
@@ -3,7 +3,10 @@ package com.link.newsfeed.NewsFeed.MainScreen
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v4.app.FragmentTransaction
+import android.view.Menu
+import android.view.MenuItem
 import com.link.newsfeed.Data.Constant
+import com.link.newsfeed.Helper.Utils
 import com.link.newsfeed.NewsFeed.HomeScreen.HomeFragment
 import com.link.newsfeed.R
 
@@ -28,5 +31,21 @@ class MainActivity : AppCompatActivity() {
         transaction?.replace(R.id.main_frame_layout, homeFragment, Constant.FragmentsTags.HOME_TAG)
         transaction?.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
         transaction?.commit()
+    }
+
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.main_meun, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val id = item.getItemId()
+        if (id == R.id.action_search) {
+            Utils.makeToast(this, "SEARCH")
+        }
+
+        return super.onOptionsItemSelected(item)
+
     }
 }
